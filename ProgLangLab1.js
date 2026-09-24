3a7a09e695bc3dc6d2ac3aad3ab8838e82ff3493
eghirbn <ejg9368@mavs.uta.edu>
2026-09-24 00:49:29 +0000

    committing basis of evenMults7 and add, filtering odd mults 5

--- ProgLangLab1.js
+++ ProgLangLab1.js
@@ -24,10 +24,41 @@ console.log(thirteenTable);
 const squaresTable = inputTable.map(Math.sqrt);
 console.log(squaresTable);
 
-//filtering odd multiples of 5 with filter()
-function checkOddMultiplesOf5(input){
-   return input%2 != 0;
+//filtering and adding odd multiples of 5 with filter() and reduce
+
+function checkOdd(input){
+   return (input%2 != 0);
 }
 
-const oddMults5 = inputTable.filter(checkOddMultiplesOf5);
+const oddMults5 = fiveTable.filter(checkOdd);
+
 console.log(oddMults5);
+
+//filtering and adding odd multiples of 7 with filter() and reduce
+function checkEven(input){
+   return (input%2 == 0);
+}
+
+function sumUp(total, input){
+   return total +input;
+}
+
+const evenMults7 = inputTable.map(input => input*=7 ).filter(checkEven).reduce(sumUp, 0);
+console.log(evenMults7);
+
+/*getting even multiples of 7
+function sumEvenMultiplesOf7(total, input){
+   if(input%2 == 0){
+      return total + input;
+   }
+   return total;
+}
+
+function sevenTableFunction(input){
+   return input*=7;
+}
+
+const sevenTable = inputTable.map(sevenTableFunction);
+
+//console.log(sevenTable.reduce(sumEvenMultiplesOf7, 0))
+*/
